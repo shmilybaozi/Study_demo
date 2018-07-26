c6eeda54089ff99c242b64f5b3d4d1dc5ee05284
shmilybaozi <shmilybaozi@163.com>
2018-07-26 18:07:17 +0800

    fixed 5.ts

--- TypeScript/1.几种类型.ts
+++ TypeScript/1.几种类型.ts
@@ -18,7 +18,7 @@ I'll be ${myAge + 1} years old next month.`
 // 空值
 // JavaScript 没有空值（Void）的概念，在 TypeScript 中，可以用 void 表示没有任何返回值的函数：
 function alertName(): void {
-alert('My name is Tom');
+  alert('My name is Tom');
 }
 // 声明一个  void  类型的变量没有什么用，因为你只能将它赋值为 undefined 和 null ：
 let unusable: void = undefined;
--- TypeScript/5.类型断言.ts
+++ TypeScript/5.类型断言.ts
@@ -1,11 +1,21 @@
 // 语法
 // <类型>值 或 值 as 类型
-function getLength(something: string | number): number {
+
+function getLength1(something: string | number): number {
   if ((<string>something).length) {
     return (<string>something).length;
   } else {
     return something.toString().length;
   }
 }
+
+function getLength2(something: string | number): number {
+  if ((something as string).length) {
+    return (something as string).length;
+  } else {
+    return something.toString().length;
+  }
+}
+// 在 tsx 语法（React 的 jsx 语法的 ts 版）中必须用后一种
 // 类型断言的用法如上，在需要断言的变量前加上 <Type> 即可。
 // 类型断言不是类型转换，断言成一个联合类型中不存在的类型是不允许的
