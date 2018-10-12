07c8361b9ed132186ad3f585a0f3646d061187e0
shmilybaozi <shmilybaozi@163.com>
2018-10-12 15:35:54 +0800

    completed 作业3

--- LessonHomework/作业3/作业3.js
+++ LessonHomework/作业3/作业3.js
@@ -21,6 +21,7 @@ var sum = function (array) {
   return s
 }
 
+
 // 作业 1
 // 参数是一个只包含数字的 array
 // 求 array 的乘积
@@ -39,12 +40,14 @@ var product = function (array) {
   return s
 }
 
-
 // 作业 2
 // 返回一个数的绝对值
 // 函数定义是
 var abs = function (n) {
-
+  if (n < 0) {
+    return -n
+  }
+  return n
 }
 
 
@@ -53,7 +56,8 @@ var abs = function (n) {
 // 求 array 中所有数字的平均数
 // 函数定义是
 var average = function (array) {
-
+  var average = sum(array) / array.length
+  return average
 }
 
 
@@ -62,23 +66,20 @@ var average = function (array) {
 // 求 array 中最小的数字
 // 函数定义是
 var min = function (array) {
-
-}
-
-var ensureEqual = function (a, b, message) {
-  if (a != b) {
-    console.log(message, a, b)
+  // 先设置一个变量用来存 第一个数
+  var min = array[0]
+  // 遍历数组
+  for (var i = 0; i < array.length; i++) {
+    // 用变量 n 保存元素值
+    var n = array[i]
+    // 比较
+    if (n < min) {
+      min = n
+    }
   }
+  // 循环结束, 现在 s 里面存的是数组中所有元素的和了
+  return min
 }
-
-var testMin = function () {
-  ensure(min([1, 2, 3]) == 1, '123 is not 1')
-  ensure(min([5, 2, 3]) == 2, '523 is not 2')
-  ensureEqual(min([5, 2, 4]), 2, '524 != 2')
-}
-
-// console.log('test min')
-// testMin()
 
 // 作业 5
 /*
@@ -87,7 +88,18 @@ var testMin = function () {
 1 - 2 + 3 - 4 + 5 ... n
 */
 var sum1 = function (n) {
-
+  // 先设置一个变量用来存 和
+  var s = 0
+  // 遍历数组
+  for (var i = 1; i <= n; i++) {
+    if (i % 2 === 0) {
+      s -= i
+    } else {
+      s += i
+    }
+  }
+  // 循环结束, 现在 s 里面存的是数组中所有元素的和了
+  return s
 }
 
 
@@ -98,7 +110,18 @@ var sum1 = function (n) {
 1 + 2 - 3 + 4 - ... n
 */
 var sum2 = function (n) {
-
+  // 先设置一个变量用来存 和
+  var s = 1
+  // 遍历数组
+  for (var i = 2; i <= n; i++) {
+    if (i % 2 !== 0) {
+      s -= i
+    } else {
+      s += i
+    }
+  }
+  // 循环结束, 现在 s 里面存的是数组中所有元素的和了
+  return s
 }
 
 
@@ -108,10 +131,38 @@ var sum2 = function (n) {
 接受一个参数 n
 返回 n 的阶乘, 1 * 2 * 3 * ... * n
 */
-var fac = function (n) {
 
+// 递归
+var fac = function (n) {
+  // 如果 n 是 0 返回 1
+  // 这是递归的终止条件, 必须要有, 否则就无限递归了
+  if (n === 0) {
+    return 1
+  } else {
+    return n * fac(n - 1)
+  }
 }
 
+// 斐波那契数列 递归
+// 1 1 2 3 5 8 ...
+var fib = function (n) {
+  // 如果 n 是 1 或 2 返回 1
+  // 这是递归的终止条件, 必须要有, 否则就无限递归了
+  if (n === 1 || n === 2) {
+    return 1
+  } else {
+    return fib(n - 2) + fib(n - 1)
+  }
+}
+
+// for 循环实现
+var facFor = function (n) {
+  var s = 1
+  for (let i = 1; i <= n; i++) {
+    s *= i
+  }
+  return s
+}
 
 
 /*
@@ -125,7 +176,20 @@ a b 分别是 2 个数字
 根据 op 对 a b 运算并返回结果(加减乘除)
 */
 var apply = function (op, a, b) {
-
+  switch (op) {
+    case '+':
+      return a + b
+      break;
+    case '-':
+      return a - b
+      break;
+    case '*':
+      return a * b
+      break;
+    case '/':
+      return a / b
+      break;
+  }
 }
 
 
@@ -133,15 +197,20 @@ var apply = function (op, a, b) {
 作业 9
 实现 applyList 函数
 op 是 '+' '-' '*' '/' 其中之一
-oprands 是一个只包含数字的 array
-根据 op 对 oprands 中的元素进行运算并返回结果
+operands 是一个只包含数字的 array
+根据 op 对 operands 中的元素进行运算并返回结果
 例如, 下面的调用返回 -4
 var n = applyList('-', [3, 4, 2, 1])
 log(n)
-// 结果是 4, 用第一个数字减去所有的数字
+// 结果是 -4, 用第一个数字减去所有的数字
 */
-var applyList = function (op, oprands) {
-
+var applyList = function (op, operands) {
+  var f = operands[0]
+  for (let i = 1; i < operands.length; i++) {
+    var s = operands[i]
+    f = apply(op, f, s)
+  }
+  return f
 }
 
 
@@ -155,7 +224,20 @@ expression 是一个 array(数组), 包含了 3 个元素
 根据 op 对数字运算并返回结果(结果是 true 或者 false)
 */
 var applyCompare = function (expression) {
-
+  var op = expression[0]
+  var a = expression[1]
+  var b = expression[2]
+  switch (op) {
+    case '>':
+      return a > b
+      break;
+    case '<':
+      return a < b
+      break;
+    case '==':
+      return a == b
+      break;
+  }
 }
 
 
@@ -171,11 +253,14 @@ expression 中第一个元素是上面几题的 op, 剩下的元素是和 op 对
 根据 expression 运算并返回结果
 */
 var applyOps = function (expression) {
-
+  var op = expression[0]
+  if (op == '+' || op == '-' || op == '*' || op == '/') {
+    var operands = expression.slice(1)
+    return applyList(op, operands)
+  } else if (op == '<' || op == '>' || op == '==') {
+    return applyCompare(expression)
+  }
 }
-
-
-console.log('fe3 答案 结束')
 
 // 注意
 // 下次作业起, 会用更标准简练的方式来表示参数类型和说明
--- LessonHomework/作业3/作业3.test.js
+++ LessonHomework/作业3/作业3.test.js
@@ -5,15 +5,111 @@ var ensure = function (condition, message) {
   }
 }
 
+var ensureEqual = function (a, b, message) {
+  if (a != b) {
+    console.log(message,'| a:', a, 'b:', b)
+  }
+}
+
 // 测试sum()
 var textSum = function() {
   ensure(sum([1, 2, 3]) === 6, '[1, 2, 3] is not 6')
 }
 
+// 测试product()
+var textProduct = function() {
+  ensure(product([1, 2, 3, 4]) === 24, '[1, 2, 3, 4] is not 24')
+}
+
+// 测试abs()
+var textAbs = function() {
+  ensure(abs(3) === 3, '3 is not 3')
+  ensure(abs(-3) === 3, '-3 is not 3')
+  ensure(abs(-9) === 9, '-9 is not 9')
+  ensure(abs(0) === 0, '0 is not 0')
+}
+
+// 测试average()
+var textAverage = function() {
+  ensure(average([1, 2, 3]) === 2, '[1, 2, 3] is not 2')
+}
+
+// 测试min()
+var textMin = function() {
+  ensure(min([1, 2, 3, 0]) === 0, '[1, 2, 3, 0] is not 0')
+  ensure(min([1, 2, 3]) == 1, '[1, 2, 3] is not 1')
+  ensure(min([5, 2, 3]) == 2, '[5, 2, 3] is not 2')
+  // 推荐使用ensureEqual()
+  ensureEqual(min([5, 2, 4]), 2, '[5, 2, 4] != 2')
+}
+
+// 测试sum1()
+var textSum1 = function() {
+  ensureEqual(sum1(10), -5, 'sum1(10) is not -5')
+}
+
+// 测试sum2()
+var textSum2 = function() {
+  ensureEqual(sum2(10), 7, 'sum2(10) is not 7')
+}
+
+// 测试fac()
+var textFac = function() {
+  ensureEqual(fac(3), 6, 'fac(3) is not 6')
+}
+
+// 测试fib()
+var textFib= function() {
+  ensureEqual(fib(6), 8, 'fib(6) is not 8')
+}
+
+// 测试facFor()
+var textFacFor = function() {
+  ensureEqual(facFor(3), 6, 'facFor(3) is not 6')
+}
+
+// 测试apply()
+var textApply = function() {
+  ensureEqual(apply('+', 1, 2), 3, '1 + 2 != 3')
+  ensureEqual(apply('-', 1, 2), -1, '1 - 2 != -1')
+  ensureEqual(apply('*', 1, 2), 2, '1 * 2 != 2')
+  ensureEqual(apply('/', 4, 2), 2, '4 / 2 != 2')
+}
+
+// 测试applyList()
+var textApplyList = function() {
+  ensureEqual(applyList('-', [3, 4, 2, 1]), -4, '3 - 4 - 2 - 1 ！= -4')
+}
+
+// 测试applyCompare()
+var textApplyCompare = function() {
+  ensure(applyCompare(['==', 3, 3], '3 != 3'))
+}
+
+// 测试applyOps()
+var textApplyOps = function() {
+  ensure(applyOps(['==', 3, 3], '3 != 3'))
+  ensureEqual(applyOps(['-', 3, 4, 2, 1]), -4, '3 - 4 - 2 - 1 ！= -4')
+}
+
 // 不要在程序最外层运行任何代码, 只应该有定义, 不应该有执行
 // 主函数: 在统一的地方执行代码
 var __main = function() {
+  console.log('主函数执行')
   textSum()
+  textProduct()
+  textAbs()
+  textAverage()
+  textSum1()
+  textSum2()
+  textFac()
+  textFib()
+  textFacFor()
+  textApply()
+  textApplyList()
+  textApplyCompare()
+  textApplyOps()
+  console.log('主函数执行结束')
 }
 
 // 调用, 整个程序只有一个入口, 即: 只有一个调用
